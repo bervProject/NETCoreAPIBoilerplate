3461ea0a9ce30e0a58f2f2c118f81e1446f75b5c
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-01-17 23:54:43 +0700

    Add test and coverage

---
+++ BervProject.WebApi.Test/WeatherForecastTest.cs
@@ -0,0 +1,20 @@
+using BervProject.WebApi.Boilerplate.Controllers;
+using System;
+using System.Linq;
+using Xunit;
+
+namespace BervProject.WebApi.Test
+{
+    public class WeatherForecastTest
+    {
+        [Fact]
+        public void GetTest()
+        {
+            var controller = new WeatherForecastController();
+
+            var result = controller.Get();
+
+            Assert.Equal(5, result.Count());
+        }
+    }
+}
