9e4236937898cedfc93b094163f345bea6954f90
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2021-11-13 16:52:23 +0700

    Update EmailServiceTest.cs

--- BervProject.WebApi.Test/Services/EmailServiceTest.cs
+++ BervProject.WebApi.Test/Services/EmailServiceTest.cs
@@ -23,20 +23,32 @@ namespace BervProject.WebApi.Test.Services
             {
                 "myreceiver@receiver.com"
             };
-            var request = new SendEmailRequest()
-            {
-                ReplyToAddresses = new List<string> { "bervianto.leo@gmail.com" },
-                Message = new Message()
-                {
-                    Body = new Body(new Content("Hello World!")),
-                    Subject = new Content("Stand by me")
-                },
-                Destination = new Destination(reciever),
-                Source = "support@berviantoleo.my.id"
-            };
             var sendEmailResponse = new SendEmailResponse()
             {
                 HttpStatusCode = System.Net.HttpStatusCode.OK,
+                MessageId = "random",
+                ResponseMetadata = new Amazon.Runtime.ResponseMetadata()
+            };
+            mockEmailService.Setup(x => x.SendEmailAsync(It.IsAny<SendEmailRequest>(), default))
+                .Returns(Task.FromResult(sendEmailResponse));
+            var emailService = mock.Create<EmailService>();
+            await emailService.SendEmail(reciever);
+            mockEmailService.Verify(x => x.SendEmailAsync(It.IsAny<SendEmailRequest>(), default), Times.Once());
+        }
+
+        [Fact]
+        public async Task SendEmailFailed()
+        {
+            using var mock = AutoMock.GetLoose();
+            var mockEmailService = mock.Mock<IAmazonSimpleEmailService>();
+            var logMock = mock.Mock<ILogger<EmailService>>();
+            var reciever = new List<String>
+            {
+                "myreceiver@receiver.com"
+            };
+            var sendEmailResponse = new SendEmailResponse()
+            {
+                HttpStatusCode = System.Net.HttpStatusCode.InternalServerError,
                 MessageId = "random",
                 ResponseMetadata = new Amazon.Runtime.ResponseMetadata()
             };
