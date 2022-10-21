6b675ceca7267fbec3d0f8747bb24c2080df41f8
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-10-21 12:30:03 +0700

    Add exception test

--- BervProject.WebApi.Test/Services/Azure/AzureStorageQueueServiceTest.cs
+++ BervProject.WebApi.Test/Services/Azure/AzureStorageQueueServiceTest.cs
@@ -74,6 +74,34 @@ namespace BervProject.WebApi.Test.Services.Azure
         }
 
         [Fact]
+        public void ReceiveEmptyMessageBecauseExceptionTest()
+        {
+            var azureConfig = new AzureConfiguration
+            {
+                Storage = new AzureStorage
+                {
+                    Queue = new StorageQueue
+                    {
+                        QueueName = ""
+                    }
+                }
+            };
+            using var mock = AutoMock.GetLoose(cfg => cfg.RegisterInstance(azureConfig).As<AzureConfiguration>());
+            var loggerMock = mock.Mock<ILogger<AzureStorageQueueService>>();
+            var queueServiceClient = mock.Mock<QueueServiceClient>();
+            var mockQueue = mock.Mock<QueueClient>();
+            queueServiceClient.Setup(x => x.GetQueueClient("")).Returns(mockQueue.Object);
+            mockQueue.Setup(x => x.CreateIfNotExists(null, default));
+            mockQueue.Setup(x => x.Exists(default).Value).Returns(false);
+            mockQueue.Setup(x => x.ReceiveMessage(It.IsAny<TimeSpan?>(), default).Value).Throws<Exception>();
+            var queueService = mock.Create<AzureStorageQueueService>();
+            var emptyResult = queueService.ReceiveMessage();
+            mockQueue.Verify(x => x.Exists(default), Times.Once());
+            mockQueue.Verify(x => x.ReceiveMessage(It.IsAny<TimeSpan?>(), default), Times.Never());
+            Assert.Null(emptyResult);
+        }
+
+        [Fact]
         public void SendMessageSuccessTest()
         {
             var azureConfig = new AzureConfiguration
@@ -102,7 +130,7 @@ namespace BervProject.WebApi.Test.Services.Azure
         }
 
         [Fact]
-        public void SendMessagFailedTest()
+        public void SendMessageFailedTest()
         {
             var azureConfig = new AzureConfiguration
             {
@@ -123,6 +151,34 @@ namespace BervProject.WebApi.Test.Services.Azure
             mockQueue.Setup(x => x.Exists(default).Value).Returns(false);
             var message = "boba";
             mockQueue.Setup(x => x.SendMessage(It.IsAny<string>(), default)).Returns(() => null);
+            var queueService = mock.Create<AzureStorageQueueService>();
+            var success = queueService.SendMessage(message);
+            mockQueue.Verify(x => x.Exists(default), Times.Once());
+            Assert.False(success);
+        }
+
+        [Fact]
+        public void SendMessageFailedBecauseExceptionTest()
+        {
+            var azureConfig = new AzureConfiguration
+            {
+                Storage = new AzureStorage
+                {
+                    Queue = new StorageQueue
+                    {
+                        QueueName = ""
+                    }
+                }
+            };
+            using var mock = AutoMock.GetLoose(cfg => cfg.RegisterInstance(azureConfig).As<AzureConfiguration>());
+            var loggerMock = mock.Mock<ILogger<AzureStorageQueueService>>();
+            var queueServiceClient = mock.Mock<QueueServiceClient>();
+            var mockQueue = mock.Mock<QueueClient>();
+            queueServiceClient.Setup(x => x.GetQueueClient("")).Returns(mockQueue.Object);
+            mockQueue.Setup(x => x.CreateIfNotExists(null, default));
+            mockQueue.Setup(x => x.Exists(default).Value).Returns(false);
+            var message = "boba";
+            mockQueue.Setup(x => x.SendMessage(It.IsAny<string>(), default)).Throws<Exception>();
             var queueService = mock.Create<AzureStorageQueueService>();
             var success = queueService.SendMessage(message);
             mockQueue.Verify(x => x.Exists(default), Times.Once());
