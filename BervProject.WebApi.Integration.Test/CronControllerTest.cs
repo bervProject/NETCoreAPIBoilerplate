00885402e1f3a27043b5d17142064517cf726990
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2022-10-27 19:00:06 +0700

    Add cron test

---
+++ BervProject.WebApi.Integration.Test/CronControllerTest.cs
@@ -0,0 +1,73 @@
+using System.Collections.Generic;
+using BervProject.WebApi.Integration.Test.Fixtures;
+using Hangfire;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace BervProject.WebApi.Integration.Test
+{
+    [Collection("Webapp")]
+    public class CronControllerTest : IDisposable
+    {
+        private readonly WebApplicationFactory<Program> _applicationFactory;
+        private readonly List<string> _registeredCron = new List<string>();
+        private readonly List<string> _registeredRecurring = new List<string>();
+        public CronControllerTest(WebAppFixture webAppFixtures)
+        {
+            this._applicationFactory = webAppFixtures.WebApp;
+        }
+
+        public void Dispose()
+        {
+            RemoveBackgroundJob();
+            RemoveRecurringJob();
+        }
+
+        private void RemoveBackgroundJob() 
+        {
+            var cronClient = (IBackgroundJobClient?)this._applicationFactory.Services.GetService(typeof(IBackgroundJobClient));
+            if (cronClient != null)
+            {
+                foreach (var cronId in _registeredCron)
+                {
+                    cronClient.Delete(cronId);
+                }
+            }
+        }
+
+        private void RemoveRecurringJob()
+        {
+            var cronClient = (IRecurringJobManager?)this._applicationFactory.Services.GetService(typeof(IRecurringJobManager));
+            if (cronClient != null)
+            {
+                foreach (var cronId in _registeredRecurring)
+                {
+                    cronClient.RemoveIfExists(cronId);
+                }
+            }
+        }
+
+        [Fact]
+        public async Task SuccessCreateCronOnceTest()
+        {
+            var client = _applicationFactory.CreateClient();
+            var response = await client.PostAsync("/api/v1.0/cron/CreateCronOnce", null);
+            Assert.True(response.IsSuccessStatusCode);
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            Assert.NotEmpty(stringResponse);
+            Console.WriteLine(stringResponse);
+            _registeredCron.Add(stringResponse);
+        }
+
+        [Fact]
+        public async Task SuccessCreateRecuranceTest()
+        {
+            var client = _applicationFactory.CreateClient();
+            var response = await client.PostAsync("/api/v1.0/cron/CreateRecurance", null);
+            Assert.True(response.IsSuccessStatusCode);
+            var stringResponse = await response.Content.ReadAsStringAsync();
+            Assert.NotEmpty(stringResponse);
+            Console.WriteLine(stringResponse);
+            _registeredRecurring.Add(stringResponse);
+        }
+    }
+}
